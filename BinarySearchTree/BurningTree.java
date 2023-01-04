48f5d4fb22208adb59e3adff274fcb0ee3970add
YUVRAJ KOROTANA <YUVRAJKOROTANA1999@GMAIL.COM>
2023-01-03 19:34:29 -0600

    Burning Tree BST Jan 3, 2023

---
+++ BinarySearchTree/BurningTree.java
@@ -0,0 +1,180 @@
+//Initial Template for Java
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node left;
+	Node right;
+
+	Node(int data) {
+		this.data = data;
+		left = null;
+		right = null;
+	}
+}
+
+class GfG {
+
+	static Node buildTree(String str) {
+
+		if (str.length() == 0 || str.charAt(0) == 'N') {
+			return null;
+		}
+
+		String ip[] = str.split(" ");
+		// Create the root of the tree
+		Node root = new Node(Integer.parseInt(ip[0]));
+		// Push the root to the queue
+
+		Queue<Node> queue = new LinkedList<>();
+
+		queue.add(root);
+		// Starting from the second element
+
+		int i = 1;
+		while (queue.size() > 0 && i < ip.length) {
+
+			// Get and remove the front of the queue
+			Node currNode = queue.peek();
+			queue.remove();
+
+			// Get the current node's value from the string
+			String currVal = ip[i];
+
+			// If the left child is not null
+			if (!currVal.equals("N")) {
+
+				// Create the left child for the current node
+				currNode.left = new Node(Integer.parseInt(currVal));
+				// Push it to the queue
+				queue.add(currNode.left);
+			}
+
+			// For the right child
+			i++;
+			if (i >= ip.length)
+				break;
+
+			currVal = ip[i];
+
+			// If the right child is not null
+			if (!currVal.equals("N")) {
+
+				// Create the right child for the current node
+				currNode.right = new Node(Integer.parseInt(currVal));
+
+				// Push it to the queue
+				queue.add(currNode.right);
+			}
+			i++;
+		}
+
+		return root;
+	}
+
+	static void printInorder(Node root) {
+		if (root == null)
+			return;
+
+		printInorder(root.left);
+		System.out.print(root.data + " ");
+
+		printInorder(root.right);
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+
+		while (t > 0) {
+			String s = br.readLine();
+			int target = Integer.parseInt(br.readLine());
+			Node root = buildTree(s);
+
+			Solution g = new Solution();
+			System.out.println(g.minTime(root, target));
+			t--;
+
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    /*class Node {
+    	int data;
+    	Node left;
+    	Node right;
+
+    	Node(int data) {
+    		this.data = data;
+    		left = null;
+    		right = null;
+    	}
+    }*/
+
+    static int res = 0;
+    static HashMap<Integer,Integer> m = new HashMap<>();
+
+    static int fillmap(Node root){
+        if (root == null){
+            return 0;
+        }
+        int l = fillmap(root.left);
+        int r = fillmap(root.right);
+        m.put(root.data,1+Math.max(l,r));
+
+        return 1+Math.max(l,r);
+
+    }
+
+    static int traverse(Node root,int target){
+        if  (root == null){
+            return 0;
+        }
+        if  (root.data == target){
+            return 1;
+        }
+        int val = traverse(root.left,target);
+        if  (val != 0){
+            if (root.right != null){
+                res = Math.max(res,val + m.get(root.right.data));
+            }
+
+            return val+1;
+
+        }
+        val = traverse(root.right,target);
+        if  (val != 0){
+            if (root.left != null){
+                res = Math.max(res,val + m.get(root.left.data));
+            }
+
+            return val+1;
+        }
+        return 0;
+
+    }
+
+    public static int minTime(Node root, int target)
+    {
+        res =0;
+        fillmap(root);
+        traverse(root,target);
+        m.clear();
+        return res;
+        // Your code goes here
+    }
+}
