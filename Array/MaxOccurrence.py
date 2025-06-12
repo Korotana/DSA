815e7038c5cfae269b2959f535a8589fa213a571
YUVRAJ KOROTANA <YUVRAJKOROTANA1999@GMAIL.COM>
2025-06-12 01:03:32 -0600

    solved and tested maxoccuring element

--- Array/MaxOccurrence.py
+++ Array/MaxOccurrence.py
@@ -7,9 +7,34 @@ def maxCountElement(arr):
         if cur_max_count == 0:
             cur_max_element = num
             cur_max_count += 1
+        elif cur_max_element == num:
+            cur_max_count += 1
         else:
             cur_max_count -= 1
 
     return {"Element": cur_max_element}
 
-print(maxCountElement([0,0,1,2,2,0,1,2,2,1,1,1]))
+print(maxCountElement([3,3,4]))
+
+
+# slight variation
+#
+#
+#
+# def majorityElement(self, arr):
+#     # code here
+#     cur_max_element = arr[0]
+#     cur_max_count = 0
+#
+#     for num in arr:
+#         if cur_max_count == 0:
+#             cur_max_element = num
+#             cur_max_count += 1
+#         elif cur_max_element == num:
+#             cur_max_count += 1
+#         else:
+#             cur_max_count -= 1
+#
+#     if arr.count(cur_max_element) > len(arr) / 2:
+#         return cur_max_element
+#     return -1
